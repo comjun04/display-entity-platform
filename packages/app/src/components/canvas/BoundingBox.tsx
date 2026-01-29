3c334c65c5907d4ca97fc3812fe785a4c3cc981f
comjun04 <comjun04@gmail.com>
2026-01-29 14:34:03 +0000

    fix(app/instancedmesh): fix target object world matrix being reset or not updated when selected

--- packages/app/src/components/canvas/BoundingBox.tsx
+++ packages/app/src/components/canvas/BoundingBox.tsx
@@ -36,6 +36,7 @@ const BoundingBox: FC<BoundingBoxProps> = ({ object, visible, color }) => {
 
     m.decompose(object.position, object.quaternion, object.scale)
     object.updateMatrix()
+    object.updateMatrixWorld() // needed to correctly calculate world matrix after this job
 
     // Prevent the helpers from blocking rays
     boxHelperRef.current.traverse((child) => (child.raycast = () => null))
