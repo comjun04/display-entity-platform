7fa06e5bb766a81663cc390846bc4eeeea58f5b5
comjun04 <comjun04@gmail.com>
2026-06-28 08:40:08 +0000

    refactor(app): ensure strict type check for GameVersion constant

--- packages/app/src/constants.ts
+++ packages/app/src/constants.ts
@@ -5,7 +5,11 @@ export const CDNBaseUrl = import.meta.env.VITE_CDN_BASE_URL
 export const BackendHost = import.meta.env.VITE_BACKEND_HOST
 /* eslint-enable @typescript-eslint/no-unsafe-assignment */
 
-export const GameVersions = [
+export const GameVersions: {
+  id: string
+  label: string
+  datapackVersion: number
+}[] = [
   {
     id: '26.1',
     label: '26.1',
