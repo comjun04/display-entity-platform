dee26d09f5b823be05acabd073210227c01660ae
comjun04 <comjun04@gmail.com>
2026-07-05 14:48:29 +0000

    fix(app/Model): item display player head ignores transformation when loading a project
    this should have fixed in #65 but somehow it does not

--- packages/app/src/components/canvas/Model.tsx
+++ packages/app/src/components/canvas/Model.tsx
@@ -283,6 +283,7 @@ const Model: FC<ModelNewProps> = ({
       loadedMesh.scale,
     )
 
+    loadedMesh.matrixWorldNeedsUpdate = true
     invalidate()
   }, [meshLoaded, modelDataTemp, displayType, xRotation, yRotation])
 
