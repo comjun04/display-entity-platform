04c6d57785c9b3b4a0ba75a9b60db97352b3e5ba
comjun04 <comjun04@gmail.com>
2026-07-24 14:13:10 +0000

    chore(block-icon-generator): setup project

---
+++ packages/block-icon-generator/src/app.ts
@@ -0,0 +1,47 @@
+import { Hono } from 'hono'
+
+import 'dotenv/config'
+
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+
+import { fileURLToPath } from 'url'
+
+const port = parseInt(process.env.PORT ?? '3100')
+
+// initialize App
+const app = new Hono()
+
+// frontend
+app.use(
+  serveStatic({
+    root: fileURLToPath(new URL('./frontend', import.meta.url)),
+  }),
+)
+
+const server = serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  (info) => {
+    console.log(`Listening on http://localhost:${info.port}`)
+  },
+)
+
+// graceful shutdown
+process.on('SIGINT', () => {
+  console.log('Received SIGINT (Ctrl+C), Shutting down...')
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  console.log('Received SIGTERM, Gracefully shutting down...')
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
+})
