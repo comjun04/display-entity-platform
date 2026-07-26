cd1d86b3c2bb441a44b3d51e41e1041a5ffbd2d2
comjun04 <comjun04@gmail.com>
2026-07-26 14:51:37 +0000

    feat(block-icon-generator): minimal sample for rendering a isometric cube

--- packages/block-icon-generator/vite.config.ts
+++ packages/block-icon-generator/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   root: './src/frontend',
+  server: {
+    port: 5183,
+  },
   build: {
     outDir: '../../dist',
     emptyOutDir: true,
