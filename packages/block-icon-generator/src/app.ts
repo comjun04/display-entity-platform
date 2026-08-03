8fa3ff1ca48939d7b1bc0c1f0b4452333e97f368
comjun04 <comjun04@gmail.com>
2026-08-03 13:41:54 +0000

    feat(block-icon-generator): open browser when backend server is ready
    when server is ready, it should open the browser to load the frontend to process automatically.
    For development purposes, the `--no-open` or `-n` switch disables opening frontend page automatically.

--- packages/block-icon-generator/src/app.ts
+++ packages/block-icon-generator/src/app.ts
@@ -6,8 +6,23 @@ import { serve } from '@hono/node-server'
 import { serveStatic } from '@hono/node-server/serve-static'
 
 import { fileURLToPath } from 'url'
+import open from 'open'
+import { parseArgs } from 'util'
 
 const port = parseInt(process.env.PORT ?? '3100')
+
+// parse arguments
+const parsedArgs = parseArgs({
+  args: process.argv.slice(2),
+  options: {
+    'no-open': {
+      type: 'boolean',
+      short: 'n',
+      default: false,
+    },
+  },
+})
+const OpenBrowserOnStart = !parsedArgs.values['no-open']
 
 // initialize App
 const app = new Hono()
@@ -26,6 +41,11 @@ const server = serve(
   },
   (info) => {
     console.log(`Listening on http://localhost:${info.port}`)
+
+    if (OpenBrowserOnStart) {
+      // open browser automatically when server is ready
+      open(`http://localhost:${info.port}`).catch(console.error)
+    }
   },
 )
 
